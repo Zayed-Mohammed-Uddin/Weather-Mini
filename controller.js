d54ba3542a00b8a8ffd8d91dd1f36d15eb66e3e5
Zayed M. Uddin <zayed.md.uddin273@gmail.com>
2025-02-03 21:41:28 -0500

    first commit

--- controller.js
+++ controller.js
@@ -1,5 +1,8 @@
 "use strict";
 import * as model from "./model.js";
+import * as view from "./views/View.js";
+import * as config from "./config.js";
+
 const controlSearchResults = function () {
 	model.loadSearchResults();
 	console.log(model.state);
